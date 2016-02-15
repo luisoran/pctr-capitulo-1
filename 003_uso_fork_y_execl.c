48434b3f679a8202e0f0493484fb91a4ab993df1
luisoran <luisoran@gmail.com>
2016-02-15 03:54:11 +0100

    Uso de fork y execl.

---
+++ 003_uso_fork_y_execl.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int uso_fork_y_execl (void)
+{
+	pid_t childpid;
+	int n = 4, i, result;
+	char buffer[20];
+
+	for (i = 1; i <= n; i++)
+	{
+		switch(childpid = fork())
+		{
+			case 0: // Código del hijo
+				snprintf(buffer, sizeof(buffer), "%d", n);
+				result = execl("/home/saihung/git/pctr-capitulo-1-hijo/Debug/pctr-capitulo-1-hijo", "pctr-capitulo-1-hijo", buffer, NULL);
+				printf("Resulado de execl: %d\n", result);
+				break;
+		}
+	}
+
+	return 0;
+}
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 int main(void)
 {
 	//uso_basico_fork();
-	cadena_de_procesos();
+	//cadena_de_procesos();
+	uso_fork_y_execl();
 
 	return 0;
 }
